81880a076dd21fb71d19d5abaa1077e4de0940e5
= <=>
2021-05-23 16:56:12 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+import Events from './comp/events';
+import News from './comp/news';
+import Stream from './comp/stream';
+import Nav from './comp/nav';
+
+import { Row, Col, DatePicker, Alert } from 'antd';
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import moment from 'moment';
+import 'moment/locale/zh-cn';
 
-export default App;
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    this.state = {
+      day: yyyy + '-' + mm + '-' + dd,
+      stream: 'percmau',
+      showStream: false
+    };
+
+  }
+
+  onChange = ((date, dateString) => {
+    this.setState({ day: dateString })
+  });
+
+  changeStream = ((channel, show) => {
+    this.setState({
+      stream: channel,
+      showStream: show
+    })
+  })
+
+  render() {
+    const { day } = this.state;
+    return (
+      <div>
+        <Row className="navRow">
+          <Col span={3} className="colorCol"></Col>
+
+          <Col span={18} className="flexNavCol">
+            <Nav />
+          </Col>
+
+          <Col span={3} className="colorCol"></Col>
+        </Row>
+
+        <Row className="dataRow">
+          <Col span={3} className="colorCol"></Col>
+
+          <Col span={9} className="colBorderMatches">
+            <Row className="CalContainer">
+              <Col className="">
+                <DatePicker onChange={this.onChange} defaultValue={moment(day, 'YYYY-MM-DD')} />
+              </Col>
+            </Row>
+            <Events key={this.state.day} day={this.state.day} changeStream={this.changeStream} />
+          </Col>
+
+          <Col span={9} className="colBorderNews">
+            {this.state.showStream ? <Stream key={this.state.stream} channel={this.state.stream} /> : null}
+          </Col>
+
+          <Col span={3} className="colorCol"></Col>
+        </Row >
+      </div>
+    )
+  }
+}
---
+++ src/constants/API_URL.js
@@ -0,0 +1,1 @@
+export const API_URL = 'https://csgo-scores-api.herokuapp.com'
